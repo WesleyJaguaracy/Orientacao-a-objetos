9c14a0521ae735604bfb982673b240b4459869ee
Wesley Jaguaracy <79489592+WesleyJaguaracy@users.noreply.github.com>
2022-11-04 00:49:48 -0300

    Segundo commit

--- 01Conceitos/Program.cs
+++ 01Conceitos/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace _01Conceitos
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
