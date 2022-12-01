9b70d683f9de72958272431700e780686aeddca9
Wesley Jaguaracy <79489592+WesleyJaguaracy@users.noreply.github.com>
2022-11-30 23:58:53 -0300

    Alterações simplórias

--- 01Conceitos/Program.cs
+++ 01Conceitos/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
+
 namespace _01Conceitos
 {
     class Program
     {
-        static void Main(string[] arg)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
         }
     }
 }
-
-
