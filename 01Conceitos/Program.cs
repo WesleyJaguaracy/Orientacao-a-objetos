c528d33ef3851af986a7a6aa8616d29bb02e853a
Wesley Jaguaracy <79489592+WesleyJaguaracy@users.noreply.github.com>
2022-11-30 23:42:04 -0300

    Alteração simples

--- 01Conceitos/Program.cs
+++ 01Conceitos/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-/*namespace _01Conceitos
+namespace _01Conceitos
 {
     class Program
     {
@@ -8,6 +8,6 @@
             Console.WriteLine("Hello, World!");
         }
     }
-}*/
+}
 
 
