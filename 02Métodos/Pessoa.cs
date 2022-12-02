94dd7c5613896ff69b50b765da9ac064ba8cf35f
Wesley Jaguaracy <79489592+WesleyJaguaracy@users.noreply.github.com>
2022-12-02 00:14:40 -0300

    01

--- 02Métodos/Pessoa.cs
+++ 02Métodos/Pessoa.cs
@@ -2,5 +2,24 @@ using System;
 
 class Pessoa
 {
-    
+
+    // Método 01
+    public void apresentar()
+    {
+        Console.Write
+    }
+
+    // Método 02
+    public void apresentar()
+    {
+        
+    }
+
+    // Método 03
+    public void apresentar()
+    {
+        
+    }
+
+
 }
