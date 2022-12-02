09509918d6d6e63f3c8dc785ab17453af7ec9d26
Wesley Jaguaracy <79489592+WesleyJaguaracy@users.noreply.github.com>
2022-12-02 00:50:54 -0300

    Significativas

--- 03ExercicioEscolar/Program.cs
+++ 03ExercicioEscolar/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+
+namespace _03ExercicioEscolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+
+            //Instaciar objeto da classe Aluno
+            Aluno a = new Aluno();
+            a.nome = "Dênis";
+            a.nota1 = 5;
+            a.nota2 = 3;
+            a.mensagem();
+
+        }
+    }
+}
