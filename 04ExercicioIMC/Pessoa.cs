73c6a05308b9b463630b6b501b79dc8bc427114b
Wesley Jaguaracy <79489592+WesleyJaguaracy@users.noreply.github.com>
2022-12-02 01:04:52 -0300

    01

--- 04ExercicioIMC/Pessoa.cs
+++ 04ExercicioIMC/Pessoa.cs
@@ -2,5 +2,20 @@ using System;
 
 class Pessoa
 {
-    
+
+    //Atributos
+    public double peso, altura;
+
+    //Cálculo
+    public double calculo()
+    {
+        return peso / (altura * altura);
+    }
+
+    //Situação
+    public string situação()
+    {
+        
+    }
+
 }
